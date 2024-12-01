9e9c9f1f8efdf50b8f778e01e8dcd36a6f60d43b
Jake Ellington <jake@cannotilever.xyz>
2024-11-30 22:09:35 -0500

    fix formatting, remove debug print

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::cmp::Ordering;
-use std::io::{self, Error, ErrorKind};
-use std::process;
 use std::env;
 use std::fmt::{Display, Formatter};
+use std::io::{self, Error, ErrorKind};
+use std::process;
 use std::process::exit;
 
 fn main() {
@@ -17,31 +17,32 @@ fn main() {
         }
         Err(_) => {}
     }
-    let mut boxes: Vec<DistroboxInstance> = vec!();
-    match get_boxes(){
-        Ok(box_list) => {
-            boxes = box_list;
-        }
+    let mut boxes: Vec<DistroboxInstance> = match get_boxes() {
+        Ok(box_list) => box_list,
         Err(e) => {
             eprintln!("Cannot get boxes: {:?}", e);
             exit(2);
         }
-    }
+    };
     boxes.sort();
     for box_inst in boxes {
         println!("trying {}", box_inst);
-        match process::Command::new("distrobox-enter").arg(&box_inst.name).arg("--").args(args.clone()).spawn() {
+        match process::Command::new("distrobox-enter")
+            .arg(&box_inst.name)
+            .arg("--")
+            .args(args.clone())
+            .spawn()
+        {
             Ok(mut child) => {
-                println!("got ok from child");
-                match child.wait(){
+                match child.wait() {
                     Ok(status) => {
-                        if status.success(){
+                        if status.success() {
                             exit(0);
                         }
+                        // else, try next box
                     }
                     Err(_) => {
                         // does not exist in this box, try the next one
-                        continue
                     }
                 }
             }
@@ -57,17 +58,21 @@ fn main() {
 
 struct DistroboxInstance {
     name: String,
-    priority: usize
+    priority: usize,
 }
 impl TryFrom<(usize, &String)> for DistroboxInstance {
     type Error = io::Error;
 
     fn try_from(value: (usize, &String)) -> Result<DistroboxInstance, Error> {
         Ok(DistroboxInstance {
-            name: value.1.split("|").nth(1).ok_or_else(|| {
-                Error::new(ErrorKind::NotFound, "Value was not found")
-            })?.trim().to_string(),
-            priority: value.0
+            name: value
+                .1
+                .split("|")
+                .nth(1)
+                .ok_or_else(|| Error::new(ErrorKind::NotFound, "Value was not found"))?
+                .trim()
+                .to_string(),
+            priority: value.0,
         })
     }
 }
@@ -84,7 +89,7 @@ impl PartialEq<Self> for DistroboxInstance {
 }
 impl PartialOrd<Self> for DistroboxInstance {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.priority.partial_cmp(&other.priority)
+        Some(self.cmp(other))
     }
 }
 impl Ord for DistroboxInstance {
@@ -94,57 +99,56 @@ impl Ord for DistroboxInstance {
 
     fn max(self, other: Self) -> Self
     where
-        Self: Sized
+        Self: Sized,
     {
         if self.priority > other.priority {
             self
-        }
-        else { 
+        } else {
             other
         }
     }
 
     fn min(self, other: Self) -> Self
     where
-        Self: Sized
+        Self: Sized,
     {
         if self.priority < other.priority {
             self
-        }
-        else {
+        } else {
             other
         }
     }
 
     fn clamp(self, min: Self, max: Self) -> Self
     where
-        Self: Sized
+        Self: Sized,
     {
         if self.priority < min.priority {
             min
-        }
-        else if self.priority > max.priority {
+        } else if self.priority > max.priority {
             max
-        }
-        else{
+        } else {
             self
         }
     }
 }
 
 fn get_boxes() -> io::Result<Vec<DistroboxInstance>> {
-    let out = process::Command::new("/usr/bin/distrobox-list").arg("--no-color").output()?;
+    let out = process::Command::new("/usr/bin/distrobox-list")
+        .arg("--no-color")
+        .output()?;
     if !out.status.success() {
         return Err(Error::new(ErrorKind::Other, format!("{:?}", out.status)));
     }
-    let result: String;
-    match String::from_utf8(out.stdout) {
-        Ok(s) => {result = s;}
-        Err(_) => {return Err(Error::new(ErrorKind::InvalidData, "Bad UTF-8"));}
-    }
+    let result: String = match String::from_utf8(out.stdout) {
+        Ok(s) => s,
+        Err(_) => {
+            return Err(Error::new(ErrorKind::InvalidData, "Bad UTF-8"));
+        }
+    };
     // parse command output
     let lines: Vec<String> = result.lines().map(|x| x.to_string()).collect();
-    let mut boxes: Vec<DistroboxInstance> = vec!();
+    let mut boxes: Vec<DistroboxInstance> = vec![];
     for line in lines.iter().enumerate().skip(1) {
         let dbx: DistroboxInstance = DistroboxInstance::try_from(line)?;
         boxes.push(dbx);
